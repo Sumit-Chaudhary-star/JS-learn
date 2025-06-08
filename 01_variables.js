c957b3ab4e674a393921c058062b1c8a05ac4b41
Sumit-Chaudhary-star <summitchy.990@gmail.com>
2025-06-08 11:01:16 +0545

    variable

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456;
+let accountEmail = "sumit@example.com";
+var accountPassword = "password123";
+accountCity = "Hasanpur"; // This will throw an error because accountCity is not declared with let or const
+
+// accountId = 2;
+
+console.log(accountId);
