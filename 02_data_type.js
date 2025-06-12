d90ef10f69aa7d23b201bdac2fb07751e3f13645
Sumit-Chaudhary-star <summitchy.990@gmail.com>
2025-06-12 19:50:26 +0545

    data type update

---
+++ 02_data_type.js
@@ -0,0 +1,3 @@
+// "use strict"; //treat all JS code as newer version
+// alert(3+3); //show the result of 3+3 in an alert box. we are using node.js, not a browser, so this will not work
+console.log(3+3); //show the result of 3+3 in the console. this will work in node.js
